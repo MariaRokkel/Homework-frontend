98b379731b53e0485a85d867fd6959b6f4e0f64e
Maria <mararokkel@mail.ru>
2024-07-06 15:02:58 +0300

    hm4

---
+++ front2/my-app/src/components/Footer.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { Link } from 'svelte-routing';
+</script>
+
+<footer>
+    <nav class="navbar">
+        <Link to="/" class="nav-link">Home page</Link>
+        <Link to="/about" class="nav-link">About me</Link>
+        <Link to="/portfolio" class="nav-link">Portfolio</Link>
+        <Link to="/comic" class="nav-link">Comic</Link>
+    </nav>
+</footer>
+
+<style>
+    .navbar {
+        background-color: #685641;
+        color: black;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        left: 0;
+        height: 10%;
+    }
+
+    .nav-link {
+        text-decoration: none;
+        color: black;
+        padding: 10px;
+        display: block;
+        text-align: center;
+        font-size: 20px;
+    }
+
+    .nav-link:hover {
+        background-color: #B77B57;
+    }
+</style>
---
+++ front2/my-app/src/components/Home.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import { Link } from 'svelte-routing';
+</script>
+
+<section id="hi">
+    <h1>Hello! I'm <span class="colorName">Rokkel Maria Andreevna</span><br>I am a first year student at <br>Innopolis University</h1>
+    <Link to="/about" class="btn" id="button1">About me</Link>
+</section>
+
+<style>
+    body {
+        padding: 0 200px;
+    }
+    a {
+        margin: 5px;
+    }
+    #hi {
+        text-align: center;
+    }
+    .colorName {
+        color: #6F6456;
+    }
+    #button1 {
+        appearance: none;
+        border: 0;
+        border-radius: 20px;
+        background: #6F6456;
+        color: #E8DBC0;
+        padding: 10px 37px;
+        font-size: 26px;
+
+    }
+    .btn {
+        margin-left: 360px;
+        margin-top: 30px;
+    }
+</style>
+
